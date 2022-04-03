64af8c17f98cdba46b1acab62c2d92348fc05be2
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 14:25:08 +0800

    guess the correct number one time only

--- numberGuessingGame.go
+++ numberGuessingGame.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	//This code creates random number and place as variable
+	// This code creates random number and place as variable
 	rand.Seed(time.Now().UnixNano())
 	hiddenNumber := int64(rand.Intn(100))
 
-	//Insert code here
+	var number int64
+	fmt.Println("Guess a number from 1 to 100")
+	fmt.Scanln(&number)
+
+	if number > 100 {
+		fmt.Println("Game over because the secret number is", hiddenNumber)
+	}
+
+	if number == hiddenNumber {
+		fmt.Println("Nice")
+	} else {
+		fmt.Println("Wrong, the number is", hiddenNumber)
+	}
+
 }
